e8ddfe21fb926e77d9052e4c49688e59b05a5c5e
Mossaddeque Mahmood <mmahmood.ict.bd@gmail.com>
2019-10-14 21:23:04 +0200

    Assess contrast security usability in production.

---
+++ src/main/java/com/chumbok/poc/contrastsecurity/ContrastSecurityPocApplication.java
@@ -0,0 +1,51 @@
+package com.chumbok.poc.contrastsecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+@RestController
+@SpringBootApplication
+public class ContrastSecurityPocApplication {
+
+    private static final String template = "Hello, %s!";
+    private final AtomicLong counter = new AtomicLong();
+
+    public static void main(String[] args) {
+        SpringApplication.run(ContrastSecurityPocApplication.class, args);
+    }
+
+    @RequestMapping("/greeting")
+    public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
+        return new Greeting(counter.incrementAndGet(), String.format(template, name));
+    }
+
+    @RequestMapping("/redirect")
+    public String redirect(@RequestParam("url") String url) {
+        return "redirect:" + url;
+    }
+
+    static class Greeting {
+
+        private final long id;
+        private final String content;
+
+        public Greeting(long id, String content) {
+            this.id = id;
+            this.content = content;
+        }
+
+        public long getId() {
+            return id;
+        }
+
+        public String getContent() {
+            return content;
+        }
+    }
+
+}
